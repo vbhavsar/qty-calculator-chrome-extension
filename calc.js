e759e819d662b152ac222916691395e4e7ad4efd
Vishal Bhavsar <vishal.bhavsar@compass.com>
2020-10-31 00:00:37 -0400

    fn improvements

--- calc.js
+++ calc.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var calc = document.getElementById('calc');
+  let calc = document.getElementById('calc');
+  let amountE = document.getElementById("amount");
+  let priceE = document.getElementById("price");
+  let resultE = document.getElementById("result");
 
   calc.addEventListener('click', function() {
-    console.log("calculate button clicked");
-    let amount = document.getElementById("amount").value;
-    let price = document.getElementById("price").value;
+    let amount = amountE.value;
+    let price = priceE.value;
+    if (amount == 0 || price == 0) {
+      return;
+    }
     let quantity = amount / price;
-    console.log("qty: "+quantity);
-    document.getElementById("result").innerHTML = quantity;
+    resultE.innerHTML = quantity;
   }, false)
 }, false);
+
